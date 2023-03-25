9652e0521f71728fd91ee435787122e16f9d9ed5
haril <songkg7@gmail.com>
2023-03-26 00:52:26 +0900

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,5 +1,4 @@
 vim.cmd('filetype plugin indent on')
-vim.cmd('autocmd BufWritePost ~/.local/share/chezmoi/* ! chezmoi apply --source-path "%"')
 
 local ok, _ = pcall(require, "impatient")
 if not ok then
