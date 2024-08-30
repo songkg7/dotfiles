1d1da74911a561cf23944682a8802f0ce6c43448
haril song <songkg7@gmail.com>
2024-08-31 03:37:11 +0900

    add dock script

---
+++ .chezmoiscripts/darwin/dock.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+source "./dock_functions.sh"
+
+declare -a apps=(
+    '/Applications/Notion Calendar.app'
+    '/Applications/Arc.app'
+    '/System/Applications/Music.app'
+    '/Applications/Spark Desktop.app'
+    '/Applications/Warp.app'
+);
+
+declare -a folders=(
+    ~/Downloads
+);
+
+clear_dock
+enable_recent_apps_from_dock
+
+for app in "${apps[@]}"; do
+    add_app_to_dock "$app"
+done
+
+for folder in "${folders[@]}"; do
+    add_folder_to_dock $folder
+done
+
+killall Dock
